429cf1c5dcdbf5eb9b1e6469bc6713940653d594
KensukeArai <kensuke.y.arai@gmail.com>
2015-09-23 12:08:05 -0400

    ExperimentalMarkContainer added

---
+++ ExperimentalMarkContainer.py
@@ -0,0 +1,19 @@
+import EnDedirs as _edd
+import pickle
+
+class ExperimentalMarkContainer:
+    pos      = None
+    marks    = None
+
+    Nx       = None
+    Nm       = None
+    xA       = None 
+    mA       = None 
+    k        = None 
+    dt       = None
+
+    def save(self):
+        oo   = self
+        dmp = open(_edd.resFN("marks.dump", dir="bond0402", create=True), "wb")
+        pickle.dump(oo, dmp)
+        dmp.close()
