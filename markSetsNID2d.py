0c8fc94fb56548c1e5d59ebfe2626a2f3b32e4bc
Kensuke Arai <kensuke.y.arai@gmail.com>
2016-02-12 15:35:28 -0500

    NID2d
    NID2d

--- markSetsNID2d.py
+++ markSetsNID2d.py
@@ -1,6 +1,43 @@
 import numpy as _N
 
+
 def set1(mND, mA=1):
+    #  M x Npf
+    #  positions
+    #  WF
+    #  stdP
+    #  alp
+    cells          = [[[-3.5,],     #  POS
+                       [0.9,  2,   ],     #  MARK
+                       [0.27, ],  #  STDP
+                       [33*mA, ]],        #  ALPHA
+    ]        #  ALPHA
+
+
+
+    alp  = []
+    up0l = []
+    mks  = []
+    markIDs = []
+    stdP = []
+    M    = 0
+    for cll in cells:
+        up0l.extend(cll[0])
+        stdP.extend(cll[2])
+        alp.extend(cll[3])
+        for m in xrange(len(cll[0])):
+            markIDs.append(M)
+            mks.append(cll[1])
+        M += len(cll[0])
+
+    mND.M          = M
+    mND.uP0        = _N.array(up0l)
+    mND.stdPMags   = _N.array(stdP)
+    mND.alp0       = _N.array(alp)
+    mND.um0        = _N.array(mks)
+    mND.markIDs    = _N.array(markIDs)
+
+def set2(mND, mA=1):
     #  M x Npf
     #  positions
     #  WF
