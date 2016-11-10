03834642d1615c0f0e3de6b01a3965768824f79f
KensukeArai <kensuke.y.arai@gmail.com>
2016-11-10 14:45:49 -0500

    Forgot to commit

--- hc_bcast.pyx
+++ hc_bcast.pyx
@@ -42,3 +42,27 @@ def hc_bcast1_par(double[:, ::1] fr, double [:, ::1] xASr, double[:, ::1] iq2r, 
             mN = m*N
             for 0 <= n < N:
                 p_qdrSPC[mN+n] = (p_fr[m] - p_xASr[n])*(p_fr[m] - p_xASr[n])*p_iq2r[m]
+
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def hc_qdr_sum(double[:, ::1] pkFRr, double [:, ::1] mkNrms, double[:, ::1] qdrSpc, double [:, ::1] qdrMKS, double [:, ::1] cont, int M, int N):
+    #  fxs       M x fss   
+    #  fxrux     Nupx    
+    #  f_intgrd  Nupx
+    cdef int m, n, mN
+
+    cdef double *p_qdrSpc   = &qdrSpc[0, 0]
+    cdef double *p_pkFRr       = &pkFRr[0, 0]
+    cdef double *p_mkNrms     = &mkNrms[0, 0]
+    cdef double *p_qdrMKS     = &qdrMKS[0, 0]
+    cdef double *p_cont       = &cont[0, 0]
+    cdef double pkFRr_m, mkNrms_m
+
+    for 0 <= m < M:
+        mN = m*N
+        pkFRr_m = p_pkFRr[m]
+        mkNrms_m = p_mkNrms[m]
+        for 0 <= n < N:
+            p_cont[mN+n] = pkFRr_m + mkNrms_m - 0.5*(p_qdrSpc[mN+n] + p_qdrMKS[mN+n])
